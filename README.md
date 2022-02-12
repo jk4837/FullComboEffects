b5e24cff882178c7c38b3a3d20e77136573d6a64
Wonda <jk4837@yahoo.com.tw>
2022-02-12 19:03:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Full Combo Effects
+
+Modify the result view when getting full combo.
+![DEMO](https://github.com/jk4837/FullComboEffects/blob/master/demo.png)
+* Show a yellow banner with "FULL COMBO" text
+* Show the same effect as getting a new high score
+
+## Credits
+
+* [zoller27osu](https://github.com/zoller27osu), [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario)
+* [Lauriethefish](https://github.com/Lauriethefish), [danrouse](https://github.com/danrouse) and [Bobby Shmurner](https://github.com/BobbyShmurner) for [this template](https://github.com/Lauriethefish/quest-mod-template)
---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+#include "main.hpp"
+
+#include "GlobalNamespace/ResultsViewController.hpp"
+#include "GlobalNamespace/LevelCompletionResults.hpp"
+#include "HMUI/ImageView.hpp"
+#include "TMPro/TextMeshProUGUI.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "UnityEngine/Transform.hpp"
+
+using namespace GlobalNamespace;
+using namespace TMPro;
+
+static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+
+// Loads the config from disk using our modInfo, then returns it for use
+Configuration& getConfig() {
+    static Configuration config(modInfo);
+    config.Load();
+    return config;
+}
+
+// Returns a logger, useful for printing debug messages
+Logger& getLogger() {
+    static Logger* logger = new Logger(modInfo);
+    return *logger;
+}
+
+static void setFullComboUI(UnityEngine::Transform *transform)
+{
+    try
+    {
+        transform->get_gameObject()->GetComponentsInChildren<TextMeshProUGUI *>()->values[3]->set_text(il2cpp_utils::newcsstr("FULL COMBO"));
+    }
+    catch (const std::exception &e)
+    {
+        getLogger().log(Logging::ERROR, "Failed to set text to cleared info.");
+        getLogger().log(Logging::ERROR, std::string(e.what()));
+    }
+
+    try
+    {
+        transform->get_gameObject()->GetComponentsInChildren<HMUI::ImageView *>()->values[3]->set_color(UnityEngine::Color::get_yellow());
+    }
+    catch (const std::exception &e)
+    {
+        getLogger().log(Logging::ERROR, "Failed to set color to background of cleared info.");
+        getLogger().log(Logging::ERROR, std::string(e.what()));
+    }
+}
+
+static void setNotFullComboUI(UnityEngine::Transform *transform)
+{
+    // no need to reset text to LEVEL CLEARED
+
+    // reset background to red color
+    try
+    {
+        const auto originColor = UnityEngine::Color(1, 0.374, 0, 1);
+        transform->get_gameObject()->GetComponentsInChildren<HMUI::ImageView *>()->values[3]->set_color(originColor);
+    }
+    catch (const std::exception &e)
+    {
+        getLogger().log(Logging::ERROR, "Failed to reset color to background of cleared info.");
+        getLogger().log(Logging::ERROR, std::string(e.what()));
+    }
+}
+
+MAKE_HOOK_MATCH(Results, &ResultsViewController::SetDataToUI, void, ResultsViewController *self)
+{
+    Results(self);
+
+    if (self->levelCompletionResults->levelEndStateType == LevelCompletionResults::LevelEndStateType::Cleared)
+    {
+        if (self->levelCompletionResults->fullCombo)
+        {
+            self->newHighScore = true; // for trigger sounds and fireworks
+            setFullComboUI(self->get_transform());
+        } else {
+            setNotFullComboUI(self->get_transform());
+        }
+    }
+}
+
+// Called at the early stages of game loading
+extern "C" void setup(ModInfo& info) {
+    info.id = ID;
+    info.version = VERSION;
+    modInfo = info;
+
+    getConfig().Load(); // Load the config file
+    getLogger().info("Completed setup!");
+}
+
+// Called later on in the game loading - a good time to install function hooks
+extern "C" void load() {
+    il2cpp_functions::Init();
+
+    INSTALL_HOOK(getLogger(), Results);
+}
